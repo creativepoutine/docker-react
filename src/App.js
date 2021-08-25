63cfcf0e49f833290bac5b098e830394b274d6fb
creativepoutine <creativepoutine@gmail.com>
2021-08-24 21:43:16 -0600

    WALA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO, THIS IS COMING FROM FEATURE!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I WAS CHANGED ON THE FEATURE BRANCH!
+          Learn React
         </a>
       </header>
     </div>
